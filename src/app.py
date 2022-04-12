707280645ac31257292071d6dad5c66130a661bb
Sriram Srinivasan <sriram@us.ibm.com>
2022-04-12 13:24:47 -0700

    introducing clientip

--- src/app.py
+++ src/app.py
@@ -6,7 +6,14 @@ app = flask.Flask(__name__)
 
 def doRender(template,u_path):
     headers = flask.request.headers
-    return flask.render_template(template,zen_url = zen_url,result = headers, path = u_path, qry = flask.request.query_string, postdata=flask.request.form)
+
+    clientip = "--"
+    if flask.request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        clientip = (flask.request.environ['REMOTE_ADDR'])
+    else:
+        clientip = (flask.request.environ['HTTP_X_FORWARDED_FOR']) # if behind a proxy
+
+    return flask.render_template(template,zen_url = zen_url,result = headers, path = u_path, qry = flask.request.query_string, postdata=flask.request.form, clientip = clientip)
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>',methods = ['POST', 'GET'])
